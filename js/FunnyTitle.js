d63612b9ae86cd04071256e1d09d37d2cbd6187d
dl000213 <1418352370@qq.com>
2022-02-13 15:02:01 +0800

    Site updated: 2022-02-13 15:02:01

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -3,12 +3,12 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         $('[rel="icon"]').attr('href', "https://i.loli.net/2020/12/20/BZhvzONd8lgoFAI.png");
-        document.title = 'ヾ(❀╹◡╹)ﾉ~我藏好了哦';
+        document.title = 'ヾ(❀╹◡╹)ﾉ~请别走';
         clearTimeout(titleTime);
     }
     else {
         $('[rel="icon"]').attr('href', "https://i.loli.net/2020/12/20/BZhvzONd8lgoFAI.png");
-        document.title = '（*＾ワ＾*）被你发现啦' + OriginTitle;
+        document.title = '（*＾ワ＾*）❤你' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
         }, 2000);
