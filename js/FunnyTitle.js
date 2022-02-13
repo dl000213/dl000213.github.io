78233e715a53e5e78b9a789d95fc922560a13abb
dl000213 <1418352370@qq.com>
2022-02-13 16:15:09 +0800

    Site updated: 2022-02-13 16:15:09

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -2,12 +2,12 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "https://i.loli.net/2020/12/20/BZhvzONd8lgoFAI.png");
+        $('[rel="icon"]').attr('href', "https://s2.loli.net/2022/02/13/6wfNMdVZR4XpPmi.png");
         document.title = 'ヾ(❀╹◡╹)ﾉ~请别走';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "https://i.loli.net/2020/12/20/BZhvzONd8lgoFAI.png");
+        $('[rel="icon"]').attr('href', "https://s2.loli.net/2022/02/13/6wfNMdVZR4XpPmi.png");
         document.title = '（*＾ワ＾*）❤' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
